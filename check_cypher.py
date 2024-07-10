09bfbea8b5b08a13c5466fa1d97f465917fe60ac
Kucheriav <kucheriav.borisovich@yandex.ru>
2024-07-10 23:59:54 +0300

    init commit: fixed bugs with cypher

--- check_cypher.py
+++ check_cypher.py
@@ -1,7 +1,6 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import os
 import math
-
 
 def golden_rectangle(area):
     # Золотое сечение
@@ -20,13 +19,12 @@ def golden_rectangle(area):
 
 img_dir = 'alphabet'
 letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
+font = ImageFont.truetype('arial.ttf', 40)
 text = input().lower()
 filtered_text = ''
 for el in text:
     if el in letters:
         filtered_text += el
-
 n = len(filtered_text)
 
 rows = cols = 0
@@ -37,19 +35,22 @@ rows = cols = 0
 # print(cols, rows)
 rows, cols = golden_rectangle(n)
 
-char_width = 90
-char_height = 60
+char_width = 40
+char_height = 40
 vert_margin = 15
 img_width = char_width * cols
 img_height = (char_height + vert_margin) * rows
 img = Image.new('RGB', (img_width, img_height), (255, 255, 255))
-
+draw = ImageDraw.Draw(img)
 
 for i, letter in enumerate(filtered_text):
     if letter in letters:
-        char = Image.open(os.path.join(img_dir, f'{letter}.jpg'))
+
         x = (i % cols) * char_width
         y = (i // cols) * (char_height + vert_margin)
-        img.paste(char, (x, y))
+        draw.text((x, y), letter, font=font, fill=(0, 0, 0))
 
-img.save('encrypted_text.jpg')
+print(cols, rows)
+print(i)
+
+img.save('encrypted_text1.jpg')
